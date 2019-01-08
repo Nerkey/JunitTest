ecda534b94bdf9d54980509bda6d6a48afd8af42
曲宇杰 <1305020730@qq.com>
2019-01-08 22:43:49 +0800

    first commit

---
+++ src/main/java/com/test/junit3/Calculator.java
@@ -0,0 +1,25 @@
+package com.test.junit3;
+
+/**
+ * 数学计算
+ */
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+}
---
+++ src/test/java/com/test/junit3/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.test.junit3;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAdd() {
+    }
+
+    public void testMinus() {
+    }
+
+    public void testMultiply() {
+    }
+
+    public void testDivide() {
+    }
+}
